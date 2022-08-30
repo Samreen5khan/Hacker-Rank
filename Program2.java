bc3303da0ecc630df0932fee86097dd7b40a8b42
samreen <samreentaj028@gmail.com>
2022-08-30 15:23:08 +0530

    new prgram

--- Program2.java
+++ Program2.java
@@ -1,4 +1,3 @@
-import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -7,23 +6,25 @@ class Result{
     {
         System.out.println(arr);
     }
-
 }
 
-public class Prob2{
-    public static void main(String[] args) throws IOException
+public class Program2 {
+
+    public static void main(String[] args)
     {
-        Result res = new Result();
         Scanner scan = new Scanner(System.in);
+
+        Result res = new Result();
+
         ArrayList<Integer> arr = new ArrayList<Integer>();
 
-        System.out.println("ENTER ARRAY: ");
+        System.out.println("Enter 5 Elements: ");
         for(int i=0;i<5;i++)
         {
             arr.add(scan.nextInt());
         }
         res.solution(arr);
-        
         scan.close();
     }
+    
 }
