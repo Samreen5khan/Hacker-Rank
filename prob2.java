aa8663c8a1677bcc2973560a83f89d5af157ac80
Samreen5khan <100500531+Samreen5khan@users.noreply.github.com>
2022-09-02 21:15:16 +0530

    Create prob2.java
    one week prepration

---
+++ prob2.java
@@ -0,0 +1,39 @@
+//code from hacker rank
+//2nd problem in one week preparation
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static void miniMaxSum(List<Integer> arr) 
+    {
+    // Write your code here
+    System.out.println(arr);
+
+    }
+
+}
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            
+    //  for(int i=0;i<5;i++)
+      //{
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+        
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
