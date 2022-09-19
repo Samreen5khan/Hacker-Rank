0a4a91b3288c96abec764f73c5add68fa49b1b2d
Samreen5khan <100500531+Samreen5khan@users.noreply.github.com>
2022-09-19 18:38:21 +0530

    Create DiagonalDifference.java

---
+++ DiagonalDifference.java
@@ -0,0 +1,85 @@
+//Find difference between sums of two diagonals
+//Given a matrix of n X n. The task is to calculate the absolute difference between the sums of its diagonal.
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+    // Write your code here
+    int ld=0,rd=0,i,j;
+        for(i=0;i<arr.size();i++)
+        {
+            for(j=0;j<arr.size();j++)
+            {
+                if(i==j)
+                {
+                    ld=ld+arr.get(i).get(j);
+                }
+            }
+        }
+       for(i=0;i<arr.size();i++)
+        {
+            for(j=0;j<arr.size();j++) 
+            {
+                if((i+j)==(arr.size()-1))
+                {
+                    rd=rd+arr.get(i).get(j);
+                    //System.out.println(rd);
+                    
+                } 
+            }
+        }
+        int res=ld-rd;
+        return Math.abs(res);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
